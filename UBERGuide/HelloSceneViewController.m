36df59531950597fc84002114e306e912fca57c3
Fincher Justin <zhtsu47@me.com>
2016-01-17 05:30:25 +0800

    intro page animation

--- UBERGuide/HelloSceneViewController.m
+++ UBERGuide/HelloSceneViewController.m
@@ -11,14 +11,23 @@
 #import "CityWelcomeViewController.h"
 #import "LocationManager.h"
 #import "UBERGuide-Swift.h"
-
+#define DEGREES_TO_RADIANS(degrees)((M_PI * degrees)/180)
+#define bottomConstrainsValue 30.0
 
 @interface HelloSceneViewController ()
 @property (weak, nonatomic) IBOutlet FBShimmeringView *shimmeringView;
 
+@property (strong,nonatomic) UIImageView *roadImageView;
+@property (strong,nonatomic) UIImageView *personImageView;
+@property (strong,nonatomic) UIImageView *carBodyImageView;
+@property (strong,nonatomic) UIImageView *tireLeftImageView;
+@property (strong,nonatomic) UIImageView *tireRightImageView;
+@property (strong,nonatomic) UIView *carMainView;
+
 @end
 
 @implementation HelloSceneViewController
+@synthesize roadImageView,personImageView,carBodyImageView,tireLeftImageView,tireRightImageView,carMainView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -40,6 +49,96 @@
     [self.view addGestureRecognizer:tap];
     
     [self.navigationController.interactivePopGestureRecognizer setDelegate:nil];
+    
+    
+    [self configureBaseView];
+    [self configureAnimationView];
+}
+
+- (void)configureBaseView
+{
+    //48.64
+    roadImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, -bottomConstrainsValue + self.view.frame.size.height - self.view.frame.size.width/750*114, self.view.frame.size.width, self.view.frame.size.width/750*114)];
+    roadImageView.image = [UIImage imageNamed:@"Road.png"];
+    [self.view addSubview:roadImageView];
+    
+    
+    personImageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, self.view.frame.size.height - 549/2, 210/2, 549/2)];
+    personImageView.image = [UIImage imageNamed:@"Person"];
+    [self.view addSubview:personImageView];
+    
+    
+    carMainView = [[UIView alloc] initWithFrame:CGRectMake(self.view.frame.size.width , self.view.frame.size.height - 165.0/2 - roadImageView.frame.size.height, 377.0/2, 165.0/2)];
+    [self.view addSubview:carMainView];
+    
+    carBodyImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0 ,0, 377/2, 145/2)];
+    carBodyImageView.image = [UIImage imageNamed:@"Car Body"];
+    [carMainView addSubview:carBodyImageView];
+    
+    tireLeftImageView = [[UIImageView alloc] initWithFrame:CGRectMake(13, 46.5, 36, 36)];
+    tireRightImageView = [[UIImageView alloc] initWithFrame:CGRectMake(155, 46.5, 36, 36)];
+    tireLeftImageView.image = [UIImage imageNamed:@"Tire"];
+    tireRightImageView.image = [UIImage imageNamed:@"Tire"];
+    
+    [carMainView addSubview:tireRightImageView];
+    [carMainView addSubview:tireLeftImageView];
+    
+    
+    // Set vertical effect
+    UIInterpolatingMotionEffect *verticalMotionEffect =
+    [[UIInterpolatingMotionEffect alloc]
+     initWithKeyPath:@"center.y"
+     type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+    verticalMotionEffect.minimumRelativeValue = @(0);
+    verticalMotionEffect.maximumRelativeValue = @(20);
+    
+    // Set horizontal effect
+    UIInterpolatingMotionEffect *horizontalMotionEffect =
+    [[UIInterpolatingMotionEffect alloc]
+     initWithKeyPath:@"center.x"
+     type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+    horizontalMotionEffect.minimumRelativeValue = @(-20);
+    horizontalMotionEffect.maximumRelativeValue = @(20);
+    
+    // Create group to combine both
+    UIMotionEffectGroup *group = [UIMotionEffectGroup new];
+    group.motionEffects = @[horizontalMotionEffect, verticalMotionEffect];
+    
+    // Add both effects to your view
+    [personImageView addMotionEffect:group];
+
+}
+
+- (void)configureAnimationView
+{
+    [CATransaction begin];
+    CABasicAnimation *rotationAnimation;
+    rotationAnimation.removedOnCompletion = NO;
+    rotationAnimation.fillMode = kCAFillModeForwards;
+    rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    rotationAnimation.byValue = [NSNumber numberWithFloat:20];
+    rotationAnimation.duration = 2.5;
+    rotationAnimation.timingFunction=[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    [CATransaction setCompletionBlock:^{
+        tireLeftImageView.transform = CGAffineTransformRotate(tireLeftImageView.transform, DEGREES_TO_RADIANS(-60*32.72));
+    }];
+    
+    [tireLeftImageView.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
+    [tireRightImageView.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
+    [CATransaction commit];
+
+    
+    CABasicAnimation *carBodyAnimation;
+    carBodyAnimation=[CABasicAnimation animationWithKeyPath:@"transform.translation.x"];
+    carBodyAnimation.duration=2;
+    carBodyAnimation.repeatCount=1;
+    carBodyAnimation.autoreverses=NO;
+    carBodyAnimation.removedOnCompletion = NO;
+    carBodyAnimation.fillMode = kCAFillModeForwards;
+    carBodyAnimation.fromValue=[NSNumber numberWithFloat:0];
+    carBodyAnimation.toValue=[NSNumber numberWithFloat:-377/2];
+    [carMainView.layer addAnimation:carBodyAnimation forKey:@"animateLayer"];
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
